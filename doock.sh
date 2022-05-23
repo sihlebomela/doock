26231fbbe9a6769ef7206f8e05a676127646ff86
trinitysenpai <siphesihlebomela@gmail.com>
2022-05-23 04:33:16 +0200

    ✨ quit script
    quit script by entering `q` option

--- doock.sh
+++ doock.sh
@@ -28,7 +28,8 @@ echo
 
 # MENU
 echo "${yellow}(1) disable ubuntu dock
-${cyan}(2) enable ubuntu dock"
+${cyan}(2) enable ubuntu dock
+${red}(q) quit (anytime i ask)${reset}"
 echo
 
 # Ask till choice is valid
@@ -62,6 +63,11 @@ while read -r OPTION; do
         }
         echo
         echo -e "Use ${black}${bgGreen}Alt + F2$reset then in the input that appears enter ${bgGreen}${black} r ${reset} to refresh gnome or reboot to see changes"
+        exit 0
+
+    # quit
+    elif [ "$OPTION" == "q" ]; then
+        echo "have a nice day :)"
         exit 0
     else
         # invalid option
